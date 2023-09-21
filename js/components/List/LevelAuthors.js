b52398e8373e587e89deecfd7c1aa1ffede891a7
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2023-09-21 17:41:05 +0500

    test 1

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -15,20 +15,10 @@ export default {
     },
     template: `
         <div class="level-authors">
-            <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-            </template>
-            <template v-else-if="creators.length === 0">
+            <template v-if="creators.length === 0">
                 <div class="type-title-sm">Creator</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
-                </p>
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
                 </p>
             </template>
             <template v-else>
@@ -39,11 +29,18 @@ export default {
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
-                </p>
             </template>
+            <div class="type-title-sm">
+                <template v-if="isVerified == true">
+                    Verifier
+                </template>
+                <template v-else>
+                    To be verified by
+                </template>
+            </div>
+            <p class="type-body">
+                <span>{{ verifier }}</span>
+            </p>
             <div class="type-title-sm">Publisher</div>
             <p class="type-body">
                 <span>{{ author }}</span>
