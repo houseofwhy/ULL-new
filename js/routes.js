8e5ce00eaaffe2c68026cb434a8335ecbe89d7d5
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2023-09-29 20:15:42 +0500

    unlisted page

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Unlisted from './pages/Unlisted.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/unlisted', component: Unlisted }
 ];
