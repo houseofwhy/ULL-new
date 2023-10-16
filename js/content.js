87a9a331151562b36d7a6d97523694c4287a650f
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2023-10-16 19:20:15 +0500

    leaderboard test

--- js/content.js
+++ js/content.js
@@ -68,67 +68,67 @@ export async function fetchLeaderboard() {
         }
 
         // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
-            completed: [],
-            progressed: [],
+        const author = Object.keys(scoreMap).find(
+            (u) => u.toLowerCase() === level.author.toLowerCase(),
+        ) || level.author;
+        scoreMap[author] ??= {
+            totalScore: 0,
+            created: []
         };
-        const { verified } = scoreMap[verifier];
-        verified.push({
+        scoreMap[author].created.push({
             rank: rank + 1,
             level: level.name,
             score: score(rank + 1, 100, level.percentToQualify),
-            link: level.verification,
+            link: level.showcase
         });
+        scoreMap[author].totalScore += score(rank + 1, 100, level.percentToQualify)
 
-        // Records
-        level.records.forEach((record) => {
-            const user = Object.keys(scoreMap).find(
-                (u) => u.toLowerCase() === record.user.toLowerCase(),
-            ) || record.user;
-            scoreMap[user] ??= {
-                verified: [],
-                completed: [],
-                progressed: [],
-            };
-            const { completed, progressed } = scoreMap[user];
-            if (record.percent === 100) {
-                completed.push({
-                    rank: rank + 1,
-                    level: level.name,
-                    score: score(rank + 1, 100, level.percentToQualify),
-                    link: record.link,
-                });
-                return;
-            }
-
-            progressed.push({
-                rank: rank + 1,
-                level: level.name,
-                percent: record.percent,
-                score: score(rank + 1, record.percent, level.percentToQualify),
-                link: record.link,
-            });
-        });
+        // // Records
+        // level.records.forEach((record) => {
+        //     const user = Object.keys(scoreMap).find(
+        //         (u) => u.toLowerCase() === record.user.toLowerCase(),
+        //     ) || record.user;
+        //     scoreMap[user] ??= {
+        //         verified: [],
+        //         completed: [],
+        //         progressed: [],
+        //     };
+        //     const { completed, progressed } = scoreMap[user];
+        //     if (record.percent === 100) {
+        //         completed.push({
+        //             rank: rank + 1,
+        //             level: level.name,
+        //             score: score(rank + 1, 100, level.percentToQualify),
+        //             link: record.link,
+        //         });
+        //         return;
+        //     }
+        //
+        //     progressed.push({
+        //         rank: rank + 1,
+        //         level: level.name,
+        //         percent: record.percent,
+        //         score: score(rank + 1, record.percent, level.percentToQualify),
+        //         link: record.link,
+        //     });
+        //});
     });
 
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
-        const { verified, completed, progressed } = scores;
-        const total = [verified, completed, progressed]
-            .flat()
-            .reduce((prev, cur) => prev + cur.score, 0);
-
-        return {
-            user,
-            total: round(total),
-            ...scores,
-        };
+      return scores;
+        // const { verified, completed, progressed } = scores;
+        // const total = [verified, completed, progressed]
+        //     .flat()
+        //     .reduce((prev, cur) => prev + cur.score, 0);
+        //
+        // return {
+        //     user,
+        //     total: round(total),
+        //     ...scores,
+        // };
     });
 
     // Sort by total score
-    return [res.sort((a, b) => b.total - a.total), errs];
+    return [res.sort((a, b) => b.totalScore - a.totalScore), errs];
 }
--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -42,52 +42,7 @@ export default {
                     </table>
                 </div>
                 <div class="player-container surface">
-                    <div class="player">
-                        <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
-                        <h3>{{ entry.total }}</h3>
-                        <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
-                        <table v-if="entry.verified.length > 0" class="table">
-                            <tr v-for="score in entry.verified">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                        <h2 v-if="entry.completed.length > 0">Completed ({{ entry.completed.length }})</h2>
-                        <table v-if="entry.completed.length > 0" class="table">
-                            <tr v-for="score in entry.completed">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                        <h2 v-if="entry.progressed.length > 0">Progressed ({{entry.progressed.length}})</h2>
-                        <table v-if="entry.progressed.length > 0" class="table">
-                            <tr v-for="score in entry.progressed">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.percent }}% {{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
+                    
                 </div>
             </div>
         </main>
