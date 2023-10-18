6b922fba1d5ea43dd6edea48b1849e72931fc12c
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2023-10-18 20:23:11 +0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -114,7 +114,7 @@ export async function fetchLeaderboard() {
         //});
     });
 
-    Wrap in extra Object containing the user and total score
+    //Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
       scores.user = user;
       return scores;
