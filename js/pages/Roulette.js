43e349c52b3b68a75738f73056296c403dd2b6d7
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2023-11-26 20:43:19 +0500

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -184,6 +184,7 @@ export default {
                 id: lvl.id,
                 name: lvl.name,
                 video: lvl.showcase,
+                leakID: lvl.leakID
             }));
             const list = [];
             if (this.useMainList) list.push(...fullListMapped.slice(0, 75));
