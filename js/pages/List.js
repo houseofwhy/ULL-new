372518b90004c1b0587265927a990bbe90f00522
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2025-03-16 19:13:27 +0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -229,21 +229,27 @@ export default {
 		useFilter(index) {
 			if(filtersList[index].separator) return
 			this.filtersList[index].active = !this.filtersList[index].active;
-			this.list.map(level => {
-				for(let filter of filtersList) {
-					if(!filter.active){
-						continue
+			this.filtersToggled = 0
+			for(let filter of filtersList) {
+				if(filter.active) filtersToggled++
+			}
+			if (!filtersToggled==0){
+				this.list.map(level => {
+					for(let filter of filtersList) {
+						if(!filter.active){
+							continue
+						}
+						if(level[0].tags == undefined || !level[0].tags.includes(filter.key)){
+							level[0].isHidden=true
+							break
+						}
+						else{
+							level[0].isHidden=false
+						}
 					}
-					if(level[0].tags == undefined || !level[0].tags.includes(filter.key)){
-						level[0].isHidden=true
-						break
-					}
-					else{
-						level[0].isHidden=false
-					}
-				}
-//				level[0].isHidden=!(this.filtersList.filter(item => item.active && level[0].tags != undefined && level[0].tags.includes(item.key))).length > 0
-			})
+	//				level[0].isHidden=!(this.filtersList.filter(item => item.active && level[0].tags != undefined && level[0].tags.includes(item.key))).length > 0
+				})
+			}
 		}
 	},
 };
