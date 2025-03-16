1fd0c5b2b8caa2e0efb9603883cef9f4598738cd
TheStxrmEz <69839911+TheStxrmEz@users.noreply.github.com>
2025-03-16 20:09:47 +0500

    leaderboard test 1

--- js/content.js
+++ js/content.js
@@ -92,6 +92,7 @@ export async function fetchUnlistedPairs() {
     return pairs;
 }
 
+
 export async function fetchLeaderboard() {
     const list = await fetchList();
 
@@ -104,70 +105,148 @@ export async function fetchLeaderboard() {
         }
 
         // Verification
-        const scoreE = Math.floor(Math.sqrt(level.percentFinished)*level.rating*level.rating*Math.sqrt(level.length)*Math.sqrt(level.rating)*45/1000*3.141592356/Math.E*1000)/1000
-        const author = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.author.toLowerCase(),
-        ) || level.author;
-        scoreMap[author] ??= {
-            totalScore: 0,
-            created: []
+        const verifier = Object.keys(scoreMap).find(
+            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+        ) || level.verifier;
+        scoreMap[verifier] ??= {
+            verified: [],
+            completed: [],
+            progressed: [],
         };
-        scoreMap[author].created.push({
+        const { verified } = scoreMap[verifier];
+        verified.push({
             rank: rank + 1,
             level: level.name,
-            score: scoreE,
-            link: level.showcase
+            score: score(rank + 1, 100, level.percentToQualify),
+            link: level.verification,
         });
-        scoreMap[author].totalScore += scoreE
 
-        // // Records
-        // level.records.forEach((record) => {
-        //     const user = Object.keys(scoreMap).find(
-        //         (u) => u.toLowerCase() === record.user.toLowerCase(),
-        //     ) || record.user;
-        //     scoreMap[user] ??= {
-        //         verified: [],
-        //         completed: [],
-        //         progressed: [],
-        //     };
-        //     const { completed, progressed } = scoreMap[user];
-        //     if (record.percent === 100) {
-        //         completed.push({
-        //             rank: rank + 1,
-        //             level: level.name,
-        //             score: score(rank + 1, 100, level.percentToQualify),
-        //             link: record.link,
-        //         });
-        //         return;
-        //     }
-        //
-        //     progressed.push({
-        //         rank: rank + 1,
-        //         level: level.name,
-        //         percent: record.percent,
-        //         score: score(rank + 1, record.percent, level.percentToQualify),
-        //         link: record.link,
-        //     });
-        //});
+        // Records
+        level.records.forEach((record) => {
+            const user = Object.keys(scoreMap).find(
+                (u) => u.toLowerCase() === record.user.toLowerCase(),
+            ) || record.user;
+            scoreMap[user] ??= {
+                verified: [],
+                completed: [],
+                progressed: [],
+            };
+            const { completed, progressed } = scoreMap[user];
+            if (record.percent === 100) {
+                completed.push({
+                    rank: rank + 1,
+                    level: level.name,
+                    score: score(rank + 1, 100, level.percentToQualify),
+                    link: record.link,
+                });
+                return;
+            }
+
+            progressed.push({
+                rank: rank + 1,
+                level: level.name,
+                percent: record.percent,
+                score: score(rank + 1, record.percent, level.percentToQualify),
+                link: record.link,
+            });
+        });
     });
 
-    //Wrap in extra Object containing the user and total score
+    // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
-      scores.user = user;
-      return scores;
-        // const { verified, completed, progressed } = scores;
-        // const total = [verified, completed, progressed]
-        //     .flat()
-        //     .reduce((prev, cur) => prev + cur.score, 0);
-        //
-        // return {
-        //     user,
-        //     total: round(total),
-        //     ...scores,
-        // };
+        const { verified, completed, progressed } = scores;
+        const total = [verified, completed, progressed]
+            .flat()
+            .reduce((prev, cur) => prev + cur.score, 0);
+
+        return {
+            user,
+            total: round(total),
+            ...scores,
+        };
     });
-    for (var i in res) console.log(res[i])
-    var sortFunce = function(a,b) { return b.totalScore-a.totalScore }
+
     // Sort by total score
-    return [res.sort(sortFunce), errs]
+    return [res.sort((a, b) => b.total - a.total), errs];
 }
+
+
+// export async function fetchLeaderboard() {
+//     const list = await fetchList();
+
+//     const scoreMap = {};
+//     const errs = [];
+//     list.forEach(([level, err], rank) => {
+//         if (err) {
+//             errs.push(err);
+//             return;
+//         }
+
+//         // Creator Leaderboard
+//         // const scoreE = Math.floor(Math.sqrt(level.percentFinished)*level.rating*level.rating*Math.sqrt(level.length)*Math.sqrt(level.rating)*45/1000*3.141592356/Math.E*1000)/1000
+//         // const author = Object.keys(scoreMap).find(
+//         //     (u) => u.toLowerCase() === level.author.toLowerCase(),
+//         // ) || level.author;
+//         // scoreMap[author] ??= {
+//         //     totalScore: 0,
+//         //     created: []
+//         // };
+//         // scoreMap[author].created.push({
+//         //     rank: rank + 1,
+//         //     level: level.name,
+//         //     score: scoreE,
+//         //     link: level.showcase
+//         // });
+//         // scoreMap[author].totalScore += scoreE
+
+//         // Records
+//         level.records.forEach((record) => {
+//             const user = Object.keys(scoreMap).find(
+//                 (u) => u.toLowerCase() === record.user.toLowerCase(),
+//             ) || record.user;
+//             scoreMap[user] ??= {
+//                 verified: [],
+//                 completed: [],
+//                 progressed: [],
+//             };
+//             const { completed, progressed } = scoreMap[user];
+//             if (record.percent === 100) {
+//                 completed.push({
+//                     rank: rank + 1,
+//                     level: level.name,
+//                     score: score(rank + 1, 100, level.percentToQualify),
+//                     link: record.link,
+//                 });
+//                 return;
+//             }
+        
+//             progressed.push({
+//                 rank: rank + 1,
+//                 level: level.name,
+//                 percent: record.percent,
+//                 score: score(rank + 1, record.percent, level.percentToQualify),
+//                 link: record.link,
+//             });
+//         });
+//     });
+
+//     //Wrap in extra Object containing the user and total score
+//     const res = Object.entries(scoreMap).map(([user, scores]) => {
+//       scores.user = user;
+//       return scores;
+//         // const { verified, completed, progressed } = scores;
+//         // const total = [verified, completed, progressed]
+//         //     .flat()
+//         //     .reduce((prev, cur) => prev + cur.score, 0);
+//         //
+//         // return {
+//         //     user,
+//         //     total: round(total),
+//         //     ...scores,
+//         // };
+//     });
+//     for (var i in res) console.log(res[i])
+//     var sortFunce = function(a,b) { return b.totalScore-a.totalScore }
+//     // Sort by total score
+//     return [res.sort(sortFunce), errs]
+// }
--- js/score.js
+++ js/score.js
@@ -11,21 +11,25 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
-        return 0;
-    }
-    if (rank > 75 && percent < 100) {
-        return 0;
-    }
+    // if (rank > 150) {
+    //     return 0;
+    // }
+    // if (rank > 75 && percent < 100) {
+    //     return 0;
+    // }
 
-    // Old formula
+    // Old TSL formula
     /*
     let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
-    // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    // New TSL formula
+    // let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    //     ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
+    // ULL Formula
+    let score = 159*(0.996773**rank)*Math.sqrt(percent)
+    
 
     score = Math.max(0, score);
 
