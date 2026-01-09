67a6306f83bab1a12a9234eff7abff19770892a2
QwidziT <grisha.yaremko@gmail.com>
2026-01-09 15:22:06 +0800

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -20,7 +20,7 @@ export default {
             <Spinner></Spinner>
         </main>
         <h2 class="type-label-lg" style="font-weight: normal; font-size: 24px; margin: 30px; padding: 0 1rem;">
-            Levels closest to verification
+            The leaderboard shows closest to verification levels
         </h2>
         <main v-else class="page-list">
             <div class="list-container surface" style="padding-block: 0rem;">
