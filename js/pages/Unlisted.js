e10defc59b5c171658dc0c7377088245cc61f4ab
QwidziT <grisha.yaremko@gmail.com>
2026-01-10 13:29:39 +0800

    format fix

--- js/pages/Unlisted.js
+++ js/pages/Unlisted.js
@@ -22,48 +22,48 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container surface">
-                                <div class="meta">
-										<h3> Level Standards </h3>
-										<p> For main list levels:</p>
-										<p> - The level must be at least rate worthy. If there is no deco, only projects with high quality layouts or famous hosts will be added </p>
-										<p> - The level should be at least 45 seconds long, since levels shorter very rarely get rated </p>
-										<p> - The level must be easier than the current Top-1 of this list and harder than the lowest level of this list </p>
-										<p> - The level must have any progress made in the last year </p>
-										<p> For unlisted levels: </p>
-										<p> - The level must be at least close to rate worthy or have sugnificant verification progress </p>
-										<p> - The level must be at least 30 seconds long</p>
-										<p> - The level must have any progress made in the last 2 years </p>
-                                        <p> <p>
-                                        <p> <p>
-								</div>
-								<div class="meta">
-										<h3> WR Submission Requirements </h3>
-										<p> When submitting a record, please ensure that you have the following:</p>
-										<p> - A complete playthrough of the record with no cuts (if you make cuts in your submitted video, include raw footage that doesn't have them) </p>
-										<p> - A decent amount of previous attempts (A single death at 1% is not sufficient, try to get somewhat far into the level. Everplay records are exempt from this.) </p>
-										<p> - Cheat Indicator (Not necessary but desirable) </p>
-										<p> - Fps/tps indicator (For mod menus that support one) </p>
-										<p> - In-game source audio/Clicks (Either is fine, however both are strongly recommended. If you don't have either in your submission video, attach raw footage that does) </p>
-										<p> Refer to <a href="https://docs.google.com/spreadsheets/d/1evE4nXATxRAQWu2Ajs54E6cVUqHBoSid8I7JauJnOzg/edit#gid=0"><b>this sheet</b></a> for a complete list of allowed mods.</p>
-										<p> Please also check for the following:</p>
-										<p> - Make sure you beat the level displayed on the site (for reference, check the level ID to ensure you're playing the correct level</p>
-										<p> - Do not use secret routes or bug routes</p>
-										<p> - Do not use easy modes, only a record of the unmodified level qualifies</p>
-								</div>
+                <div class="meta">
+                        <h3> Level Standards </h3>
+                        <p> For main list levels:</p>
+                        <p> - The level must be at least rate worthy. If there is no deco, only projects with high quality layouts or famous hosts will be added </p>
+                        <p> - The level should be at least 45 seconds long, since levels shorter very rarely get rated </p>
+                        <p> - The level must be easier than the current Top-1 of this list and harder than the lowest level of this list </p>
+                        <p> - The level must have any progress made in the last year </p>
+                        <p> For unlisted levels: </p>
+                        <p> - The level must be at least close to rate worthy or have sugnificant verification progress </p>
+                        <p> - The level must be at least 30 seconds long</p>
+                        <p> - The level must have any progress made in the last 2 years </p>
+                        <p> <p>
+                        <p> <p>
+                </div>
+                <div class="meta">
+                        <h3> WR Submission Requirements </h3>
+                        <p> When submitting a record, please ensure that you have the following:</p>
+                        <p> - A complete playthrough of the record with no cuts (if you make cuts in your submitted video, include raw footage that doesn't have them) </p>
+                        <p> - A decent amount of previous attempts (A single death at 1% is not sufficient, try to get somewhat far into the level. Everplay records are exempt from this.) </p>
+                        <p> - Cheat Indicator (Not necessary but desirable) </p>
+                        <p> - Fps/tps indicator (For mod menus that support one) </p>
+                        <p> - In-game source audio/Clicks (Either is fine, however both are strongly recommended. If you don't have either in your submission video, attach raw footage that does) </p>
+                        <p> Refer to <a href="https://docs.google.com/spreadsheets/d/1evE4nXATxRAQWu2Ajs54E6cVUqHBoSid8I7JauJnOzg/edit#gid=0"><b>this sheet</b></a> for a complete list of allowed mods.</p>
+                        <p> Please also check for the following:</p>
+                        <p> - Make sure you beat the level displayed on the site (for reference, check the level ID to ensure you're playing the correct level</p>
+                        <p> - Do not use secret routes or bug routes</p>
+                        <p> - Do not use easy modes, only a record of the unmodified level qualifies</p>
+                </div>
             </div>
             <div class="level-container surface">
-								<table class="list unlisted-table" v-if="unlisted">
-										<tr v-for="pair in unlisted">
-												<td v-for="level in pair" class="unlisted-td">
-														<div class="unlisted-name type-h3">
-																<a :href="level.link">{{ level.name }}</a>
-														</div>
-														<div class="unlisted-creator type-label-lg">
-																{{level.creator}}
-														</div>
-												</td>
-										</tr>
-								</table>
+                <table class="list unlisted-table" v-if="unlisted">
+                    <tr v-for="pair in unlisted">
+                        <td v-for="level in pair" class="unlisted-td">
+                            <div class="unlisted-name type-h3">
+                                <a :href="level.link">{{ level.name }}</a>
+                            </div>
+                            <div class="unlisted-creator type-label-lg">
+                                {{level.creator}}
+                            </div>
+                        </td>
+                    </tr>
+                </table>
             </div>
             <div class="meta-container surface">
                 <div class="meta">
