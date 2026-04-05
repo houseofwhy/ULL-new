a7464d173a4322e3eb54837344dc02b20844cb9f
QwidziT <grisha.yaremko@gmail.com>
2026-04-05 15:12:01 +0800

    Update formulas.js

--- js/formulas.js
+++ js/formulas.js
@@ -7,9 +7,16 @@
  */
 export function recordScore(rank, percent) {
     if (percent <= 0 || rank <= 0) return 0;
-    const s_p = 0.7 * percent + 10;
+    let s_p;
+    if (percent <= 35) {
+        s_p = 0.05 * Math.pow(percent + 10, 2);
+    } else {
+        s_p = -0.008 * Math.pow(percent - 200, 2) + 320;
+    }
     const s_r = 1.5 * (30000 / (rank + 40) - 2);
-    let s = s_p * s_r / 60;
+
+    const s_p_base = 0.05 * Math.pow(50 + 10, 2);
+    let s = s_p * s_r / s_p_base;
     s = Math.max(0, s);
 
     return Math.round(s * 1000) / 1000;
